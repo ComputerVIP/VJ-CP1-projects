c9dc0f2cf49a7f974453746aae585d1981eb6712
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-09-09 09:15:09 -0600

    Fixed the program

--- Who are you/Main.py
+++ Who are you/Main.py
@@ -1,2 +1,5 @@
 #Vincent Johnson, Who Are You assignment
-print(input("Age, favourite colour, and name?"))
+name = input("What is your name?\n")
+age = input("What is your age?\n")
+colour = input("What is your favourite colour?\n")
+print(f"\n\nHello, {name}, your are {age} years old, and your favourite colour is: {colour}\n")
