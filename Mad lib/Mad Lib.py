3ee47993d369ce9a2ad8d78bd9d3b3d4df750a38
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-09-18 09:52:57 -0600

    Finished assignment

---
+++ Mad lib/Mad Lib.py
@@ -0,0 +1,15 @@
+
+#Vincent Johnson Mad Lib
+get = input("Name?\n")
+plc = input("Place?\n")
+desc = input("Emotion?\n")
+clr = input("Colour?\n")
+tme = input("Time?\n")
+nme = input("Name?\n")
+
+frst = (f"Me and {get} went to the {plc}.")
+scnd = (f"It was so {desc} and {clr}!")
+thrd = (f"It was about {tme} at {plc}.")
+frth = (f"It being {clr} surprised me!")
+fth = (f"Me and {get} and {nme} had so much {desc}!")
+print(f"{frst}\n{scnd}\n{thrd}\n{frth}\n{fth}")
