579bb9cd978772ca2edca86a1e5b41e42b835c8d
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-09-18 09:38:11 -0600

    Completed it, stops after 10 seconds

---
+++ Fibbonachi/Vincent J Fibbonachi.py
@@ -0,0 +1,10 @@
+import time
+tme = int(time.time())
+print(tme)
+fib = [0,1]
+while True:
+    tm = int(time.time())
+    fib.append(fib[-2]+fib[-1])
+    print(fib)
+    if round(tm) >= round(tme+10):
+        break
