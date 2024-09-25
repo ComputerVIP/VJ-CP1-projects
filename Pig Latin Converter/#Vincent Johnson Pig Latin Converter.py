12ac68300f6d073225422632f3a9726ed7013354
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-09-25 09:17:47 -0600

    Finished assignment

--- Pig Latin Converter/#Vincent Johnson Pig Latin Converter.py
+++ Pig Latin Converter/#Vincent Johnson Pig Latin Converter.py
@@ -1,17 +1,24 @@
 #Vincent Johnson Pig Latin Converter
 #Write a function that will take in any word and convert it into pig latin
 x = 0
-var3 = 0
+var0 = ""
+var3 = ""
 var = input("What is the word you want to convert?\n")
 var1 = [i for i in var]
-leng = len(var1)
+leng = str(len(var1))
+leng2 = int(leng)
 var2 = str(var1[0][0])
 var2 = var2.lower()
 def end():
     var4 = var3 + var2
-    print(f"\n{var1}\n{leng}\n {var4}")
+    var4 = var4 + "ay"
+    var4 = var4.capitalize()
+    print(f"{var4}")
 while True:
     x +=1
-    var3 = var1[x][0]
-    if x < leng:
+    xy = int(x)
+    var0 = var1[xy][0]
+    var3 = var3 + var0
+    if x >= (leng2-1):
         end()
+        break
