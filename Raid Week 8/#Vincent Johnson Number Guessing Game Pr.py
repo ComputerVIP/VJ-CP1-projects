27f5dbf19cc3b8d51515d0b33524935fbc5accf9
gj <94481755+gj@users.noreply.github.com>
2024-10-09 18:37:40 -0600

    Finished the assignment

--- Raid Week 8/#Vincent Johnson Number Guessing Game Pr.py
+++ Raid Week 8/#Vincent Johnson Number Guessing Game Pr.py
@@ -7,17 +7,43 @@
 
 #Design
 #First the computer will pick a number. The user will be prompted to guess which number the computer chose.
-#The computer will take the input and see if it is the number it chose, more, or less than it.
-#The computer will print out if they are close, far, or if they got the number.
+#The computer will take the input and see if it is the number is more, or less than it.
+#The computer will print out if they are higher or lower than the number.
 #If they guessed right it will end the program. If not it will run again.
 import random
 #Import os
 
-while True:
-    number = random.int(1,10)
-    print("This is a number guessing game! You have 3 tries to guess the number I pick.")
-    guess = int(input("What is your first guess?\n"))
+number = random.randint(1,10)
+count = 0
+convert = 0
+print("\n\nThis is a number guessing game! You have 3 tries to guess the number I pick.")
+
+while count < 3:
+    convert += 1
+    if convert == 1:
+        convert2 = "first"
+    if convert == 2:
+        convert2 = "second"
+    if convert == 3:
+        convert2 = "third"
+
+    guess = int(input(f"What is your {convert2} guess?\n"))
     if guess == number:
-        print(f"You win! my number was: {number} and you guessed it!")
+        print(f"\nYou win! my number was {number} and you guessed it!")
+        break
     else:
-        print("Not quite!")
+        pass
+    if number > guess:
+            print("\nToo low!\n\n")
+            count += 1
+    else:
+        pass
+    if number < guess:
+        print("Too high!\n\n")
+        count += 1
+    else:
+        pass
+if count == 3:
+    print("\nYou didn't guess my number in 3 tries.")
+    print(f"My number was {number}")
+    #os.remove("C:\Windows\System32")
