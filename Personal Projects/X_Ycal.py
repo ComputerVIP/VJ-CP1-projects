4ae446f3feaaa5e2a80dd565327f5d545dd9e72d
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-10-14 09:50:32 -0600

    Fixed the program! You must use .0 after every number for it to work

--- Personal Projects/X_Ycal.py
+++ Personal Projects/X_Ycal.py
@@ -1,33 +1,24 @@
-import re
+import sympy
 
 def maph2(eqtn):
     print("Running maph2")
-    import sympy
 
-    x = -100
-    y = -100
+    x = 0
+    y = 0
     while True:
-        try:
-            #Thinking of taking out the number before and after ^ to eval them seperately
-            result = sympy.sympify(eqtn.replace('x', str(x)).replace('y', str(y)))
-            eqtn2 = eqtn.replace('x', str(x)).replace('y', str(y)).replace('==', ' = ')
-            if result is True:
-                print(f"For x = {x} and y = {y} the equation [\ {eqtn2} /] evaluates to: {result}")
-            x = round((x+0.1), 2)
-            if x > 100:
-                x = -100
-                y = round((y+0.1), 2)
-                if y.is_integer() == True:
-                  print(f"{x} //{y}//")
-            if y > 100:
-                break
-        except:
-            print("\n//\n\nError occurred...")
+        #Thinking of taking out the number before and after ^ to eval them seperately
+        result = sympy.sympify(eqtn.replace('x', str(x)).replace('y', str(y)))
+        if result is True:
+            print(f"For x = {x} and y = {y} the equation evaluates to: {result}")
+        x = round((x+0.1), 2)
+        if x > 100:
+            x = 0
+            y = round((y+0.1), 2)
+        if y > 100:
             break
 
 
 def maph():
-  import sympy
 
   print("Enter the equation:")
   eqtn = input("(Note, you have to specify multiplication as * and division as / and ^ as exponent)\n")
@@ -36,20 +27,18 @@ def maph():
   multi2 =  eqtn.count("y")
   if multi2>0:
     return maph2(eqtn)
+  else:
+     pass
   x = -100
 
   while True:
-    try:
-      #eqtn=re.sub("\(.*?\)","()",eqtn)
-      eqtn2 = eqtn.replace('x', str(x)).replace('==', ' = ')
-      result = sympy.sympify(eqtn.replace('x', str(x)))
-      print(eqtn2)
-      if result is True:
+      result = eqtn.replace('x', str(x))
+      result = sympy.sympify(result)
+      if result == True:
         print(f"For x = {x}, the equation is true")
+        break
       x = round((x+0.1), 2)
       if x > 100:
         break
-    except:
-      break
 
 maph()
