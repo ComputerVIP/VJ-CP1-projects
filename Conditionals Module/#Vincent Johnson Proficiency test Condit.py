3a5a5b307571fd12629ede4ac5b9fd27d4455d83
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-10-15 09:20:20 -0600

    Added up to 8 names

--- Conditionals Module/#Vincent Johnson Proficiency test Condit.py
+++ Conditionals Module/#Vincent Johnson Proficiency test Condit.py
@@ -2,7 +2,7 @@
 
 name = input("What is your username?\n")
 name = name.lower()
-admin = ["admin","dev","pedro","ethan","gabe"]
+admin = ["admin","dev","pedro","ethan","gabe","sam","mr m","jimothy"]
 if name in admin:
     print("Hello admin.")
 else:
