57d7489f0d4e2c2839a6c8bd4d400eddf859145c
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-10-15 09:30:07 -0600

    Added new catagory

--- Conditionals Module/#Vincent Johnson Proficiency test Condit.py
+++ Conditionals Module/#Vincent Johnson Proficiency test Condit.py
@@ -3,7 +3,10 @@
 name = input("What is your username?\n")
 name = name.lower()
 admin = ["admin","dev","pedro","ethan","gabe","sam","mr m","jimothy"]
+users = ["nerd","void","empty","pain","anguish","darkness","sad","anger"]
 if name in admin:
     print("Hello admin.")
+elif name in users:
+    print("Hello user.")
 else:
-    print("You are not an admin.")
+    print("You are not an admin or a user.")
