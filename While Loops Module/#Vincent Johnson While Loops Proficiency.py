599b1e49457cc433ed5e8654e35366e278c811ca
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-10-23 09:36:45 -0600

    Finished the assignment

--- While Loops Module/#Vincent Johnson While Loops Proficiency.py
+++ While Loops Module/#Vincent Johnson While Loops Proficiency.py
@@ -1,14 +1,68 @@
 #Vincent Johnson While Loops Proficiency test
+import random
+
 score = 0
 games = 0
 
-def rps():
-    ans = input("Do you want to continue playing? y/n\n")
-    if ans is "y":
+def rps(score, games):
+    ans = int(input("Do you want to continue playing? y=1/n=2\n"))
+    if ans == 1:
         ans = True
-    if ans is "n":
+    elif ans == 2:
+        print("Thanks for playing!")
         ans = False
-    else:
-        print("That is not a valid answer! y/n!")
-        rps()
-    while ans != True:
+    while ans == True:
+        choice = int(input("Press 1 for paper\nPress 2 for rock\nPress 3 for scissors\n"))
+        other = random.randint(1,3)
+        if choice == 1:
+            if other == 2:
+                score += 1
+                games += 1
+                print("\nYou win!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 1:
+                games += 1
+                print("\nYou tied!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 3:
+                games += 1
+                print("\nYou lost!")
+                print(f"{score}/{games}")
+                rps(score, games)
+        elif choice == 2:
+            if other == 3:
+                score += 1
+                games += 1
+                print("\nYou win!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 2:
+                games += 1
+                print("\nYou tied!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 1:
+                games += 1
+                print("\nYou lost!")
+                print(f"{score}/{games}")
+                rps(score, games)
+        elif choice == 3:
+            if other == 1:
+                score += 1
+                games += 1
+                print("\nYou win!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 3:
+                games += 1
+                print("\nYou tied!")
+                print(f"{score}/{games}")
+                rps(score, games)
+            elif other == 2:
+                games += 1
+                print("\nYou lost!")
+                print(f"{score}/{games}")
+                rps(score, games)
+rps(score, games)
