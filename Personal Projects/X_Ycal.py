c9aa6181e0fc13d59c17b561d93f3c29eeb6b7af
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-10-28 09:52:21 -0600

    Made some changes to the code

--- Personal Projects/X_Ycal.py
+++ Personal Projects/X_Ycal.py
@@ -1,11 +1,11 @@
 import sympy
 
 def maph2(eqtn):
-    print("Running maph2")
+    #X and Y
+    print("This will take a while!")
     x = 0
     y = 0
     while True:
-        #Thinking of taking out the number before and after ^ to eval them seperately
         result = sympy.sympify(eqtn.replace('x', str(x)).replace('y', str(y)))
         if result is True:
             print(f"For x = {x} and y = {y} the equation evaluates to: {result}")
@@ -20,7 +20,6 @@ def maph():
   print("Enter the equation:")
   eqtn = input("(Note, you have to specify multiplication as * and division as / and ^ as exponent)\n")
   eqtn = eqtn.replace('=', '==').replace('^', '**')
-  print(eqtn)
   multi2 =  eqtn.count("y")
   if multi2>0:
     return maph2(eqtn)
@@ -28,11 +27,11 @@ def maph():
      pass
   x = -100
   while True:
+      #Just X
       result = eqtn.replace('x', str(x))
       result = sympy.sympify(result)
       if result == True:
         print(f"For x = {x}, the equation is true")
-        break
       x = round((x+0.1), 2)
       if x > 100:
         break
