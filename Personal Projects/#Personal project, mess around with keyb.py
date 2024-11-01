417cff8deedd1ec7e52b59f861041660c6e5ae99
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-11-01 09:51:27 -0600

    Started rick roll program

--- Personal Projects/#Personal project, mess around with keyb.py
+++ Personal Projects/#Personal project, mess around with keyb.py
@@ -1,11 +1,22 @@
 #Personal project, mess around with keyboard module
 import time
 import keyboard
+
+
 def hello():
-    time.sleep(10)
-    print("Going")
-    keyboard.send('ctrl+t')
-    keyboard.send('menu')
+    time.sleep(5)
+    keyboard.send("windows + s")
+    time.sleep(0.5)
+    keyboard.write("Microsoft Edge")
+    time.sleep(0.5)
+    keyboard.send("enter")
+    time.sleep(1)
+    keyboard.send("ctrl + t")
+    time.sleep(1)
+    keyboard.write("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    time.sleep(0.5)
+    keyboard.send("enter")
     time.sleep(10)
     return
+
 hello()
