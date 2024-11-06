165c94b7642a27aa0fc862ac825a2d9f47da6aa4
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-11-06 09:50:37 -0700

    Started a project

---
+++ Personal Projects/#This is for the map quiz in geography.py
@@ -0,0 +1,53 @@
+#This is for the map quiz in geography
+import time
+import mouse
+import keyboard
+import selenium
+from selenium import webdriver 
+from selenium.common.exceptions import NoSuchElementException 
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+url = "https://www.geoguessr.com/quiz/seterra/challenge/JM66C7"
+driver.get(url)
+
+wordsearch = "Russia"
+def findtext(wordsearch):
+    # find out if specific text is on the webpage and print result
+    if wordsearch in driver.page_source:
+        print("True")
+        time.sleep(2)
+        return
+    else:
+        print("False")
+        return
+    
+findtext(wordsearch)    
+def main():
+    time.sleep(5)
+    mouse.wheel(-3)
+    mouse.move(-10000, -10000, absolute=False, duration=0.1)
+    mouse.move(1000, 400, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(300, 70, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(0, 100, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(100, 155, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(55, 0, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(0, -40, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(50, -5, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(50, -100, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(0, -40, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(50, 0, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(50, 0, absolute=False, duration=0.1)
+    mouse.click('left')
+    mouse.move(0, 150, absolute=False, duration=0.1)
+    mouse.click('left')
