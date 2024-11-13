a4eddfb637260c2f303714d1e07f9655361f6db8
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-11-13 09:19:12 -0700

    Fixed the raid to count down instead of up

--- Raid Week of 13-11-2024/#Vincent Johnson RAID.py
+++ Raid Week of 13-11-2024/#Vincent Johnson RAID.py
@@ -3,12 +3,11 @@
 import time
 def main():
     tme = int(input("How long do you want a timer for?\n"))
-    tim = 0
-    while tim < tme:
+    while tme > 0:
         time.sleep(1)
-        tim += 1
-        print(f"You are at {tim} second(s)!")
-    if tim >= tme:
+        tme -= 1
+        print(f"You have {tme} second(s) left in the timer!")
+    if tme <= 0:
         print("Timer over!\n\n\n")
         ans = int(input("Do you want to run again? 1 = y   2 = n\n"))
         if ans == 1:
