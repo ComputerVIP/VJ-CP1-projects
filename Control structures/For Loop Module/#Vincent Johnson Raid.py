f4c035c98af598ef36e5d0f30eb3cb62aeab0710
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-11-19 09:47:25 -0700

    Finished assignment

---
+++ Control structures/For Loop Module/#Vincent Johnson Raid.py
@@ -0,0 +1,20 @@
+#Vincent Johnson Raid
+count = 0
+numbr = 1
+
+def main(count, numbr):
+    histo = "___________"
+    number = int(input("What is your number?\n"))
+    if number > 9:
+        print("9 or less!")
+        return
+    histo = histo.replace("_", "*", number)
+    print(f"{numbr}. {histo}")
+    numbr += 1
+    count += 1
+    while count <= 4:
+        main(count, numbr)
+        return
+    if count > 4:
+        return
+main(count, numbr)
