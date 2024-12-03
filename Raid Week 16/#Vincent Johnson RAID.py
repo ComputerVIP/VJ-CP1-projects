7915498900c37f91bb0ed0e3321841418cdf5a78
ComputerVIP <vincent.johnson@ucas-edu.net>
2024-12-03 09:43:03 -0700

    Fixed it...mostly

--- Raid Week 16/#Vincent Johnson RAID.py
+++ Raid Week 16/#Vincent Johnson RAID.py
@@ -4,7 +4,7 @@ num1 = 1
 num2 = 1
 row = "1 |  "
 
-print("  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10  |  11  |  12")
+print("    1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10  |  11  |  12")
 
 while count < 13:
     if num1 > 12:
@@ -16,13 +16,22 @@ while count < 13:
             row = (f"{count} |  ")
         elif len(str(count)) == 2:
             row = (f"{count}| ")
+            if count >= 10:
+                row = (f"{count}|  ")
     ans = (num1 * num2)
     ans = str(ans)
+    if count >= 10:
+        ans = ans + "  "
+    else:
+        ans = ans + " "
     row = row + ans
-    if len(ans) == 1:
-        row = row + ("  |  ")
-    elif len(ans) == 2:
+    
+    if len(ans) == 2:
         row = row + ("  | ")
     elif len(ans) == 3:
         row = row + (" | ")
+    elif len(ans) == 4:
+        row = row + ("| ")
+    elif len(ans) == 5:
+        row = row + ("|")
     num1 += 1
